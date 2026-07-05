2ceb652b7b8e16ee2465bf342ec632d807cfbe3d
Raí Onório <raionorio0204@gmail.com>
2026-07-05 14:55:07 -0300

    feat: cria tela admin da plataforma

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -11,6 +11,7 @@ import { ServicesPage } from "../pages/ServicesPage";
 import { ProfessionalsPage } from "../pages/ProfessionalsPage";
 import { AppointmentsPage } from "../pages/AppointmentsPage";
 import { PublicBookingPage } from "../pages/PublicBookingPage";
+import { AdminPage } from "../pages/AdminPage";
 
 function ProtectedPage({ children }: { children: ReactNode }) {
   return (
@@ -77,6 +78,15 @@ export function AppRoutes() {
         element={
           <ProtectedPage>
             <AppointmentsPage />
+          </ProtectedPage>
+        }
+      />
+
+      <Route
+        path="/admin"
+        element={
+          <ProtectedPage>
+            <AdminPage />
           </ProtectedPage>
         }
       />
